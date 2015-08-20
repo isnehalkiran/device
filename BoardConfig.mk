d52460aee8a2263b618c18e1a081f5ecca82cf92
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2015-08-20 10:14:28 +0530

    Enable TS MAKEUP macro.
    Change-Id: I55e9df38cc2d94892bfc70e619147527e45397f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,8 @@ TARGET_USES_NEW_ION_API :=true
 TARGET_USES_QCOM_BSP := true
 TARGET_NO_RPC := true
 
+TARGET_TS_MAKEUP := true
+
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_SEPARATED_DT := true
 
